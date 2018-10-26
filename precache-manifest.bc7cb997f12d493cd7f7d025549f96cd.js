9c4d4189062ad80a03778d3a57cb5a8bef9268d0
R-Layer <layer@europe.com>
2018-10-26 09:08:23 +0200

    Updates

---
+++ precache-manifest.bc7cb997f12d493cd7f7d025549f96cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1477ae9d7f4ecd99f8be",
+    "url": "/doit/static/js/runtime~main.1477ae9d.js"
+  },
+  {
+    "revision": "c4ea60fcc43e3145fe8b",
+    "url": "/doit/static/js/main.c4ea60fc.chunk.js"
+  },
+  {
+    "revision": "a0bd5287c21531ebed32",
+    "url": "/doit/static/js/1.a0bd5287.chunk.js"
+  },
+  {
+    "revision": "c4ea60fcc43e3145fe8b",
+    "url": "/doit/static/css/main.2b6f7f86.chunk.css"
+  },
+  {
+    "revision": "f31867a5bc9bf5537afed5dc8e56fa90",
+    "url": "/doit/index.html"
+  }
+];
